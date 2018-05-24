0fd60b55972a1aed66d6c6747fccbefa63c2c580
Lucas Avila <lucas.avila.2010@gmail.com>
2018-05-24 01:21:22 -0300

    se cambian variables

--- src/main/java/com/mercado/libre/Magneto/services/impl/ValidateADNServiceImpl.java
+++ src/main/java/com/mercado/libre/Magneto/services/impl/ValidateADNServiceImpl.java
@@ -1,8 +1,6 @@
 package com.mercado.libre.Magneto.services.impl;
 
-import com.mercado.libre.Magneto.repositories.SecuenciaADNRepository;
 import com.mercado.libre.Magneto.services.IValidateADNService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Pattern;
@@ -10,74 +8,91 @@ import java.util.regex.Pattern;
 @Service
 public class ValidateADNServiceImpl implements IValidateADNService {
 
+    private static Integer NUMERO_PARA_SER_MUTANTE=2;
+
+    /**
+     * @param dna: Array de Strings en el que se reciben las lineas de ADN
+     * @return Boolean
+     * Método que se encarga de obtener una matriz en base al Array de Strings de parámetro
+     * y calcula si es mutante en base a dicha matriz
+     * */
     public Boolean isMutant(String[] dna){
 
         String [][] matriz = getMatriz(dna);
 
         return calcularMutante(matriz);
     }
-
+    /**
+     * @param matriz: Matriz de Strings
+     * @return Boolean
+     * Calcula si tiene 2 o mas lineas de ADN retorna verdadero sino es falso
+     * */
     public Boolean calcularMutante(String [][] matriz){
-        return calculaADN(matriz)>=2 ? true : false;
+        return calculaADN(matriz)>=NUMERO_PARA_SER_MUTANTE ? true : false;
     }
 
-    public Long calculaADN(String [][] matriz){
-        Long cuenta = 0L;
+    public Integer calculaADN(String [][] matriz){
+        Integer cuenta = 0;
 
-        //cortar si encuentra 2
         for(int i=0;i<matriz.length;i++){
             for(int j=0;j<matriz.length;j++){
                 cuenta+=calculoDiagonalAsc(matriz, i, j);
-                if(cuenta>=2)
+                if(cuenta>=NUMERO_PARA_SER_MUTANTE)
                     return cuenta;
                 cuenta+=calculoFila(matriz, i, j);
-                if(cuenta>=2)
+                if(cuenta>=NUMERO_PARA_SER_MUTANTE)
                     return cuenta;
                 cuenta+=calculoDiagonalDesc(matriz, i, j);
-                if(cuenta>=2)
+                if(cuenta>=NUMERO_PARA_SER_MUTANTE)
                     return cuenta;
                 cuenta+=calculoColumna(matriz, i, j);
-                if(cuenta>=2)
+                if(cuenta>=NUMERO_PARA_SER_MUTANTE)
                     return cuenta;
             }
         }
         return cuenta;
     }
-
-    private Long calculoDiagonalAsc(String[][] matriz, int i, int j){
+    /**
+     * @param matriz: Matriz de Strings
+     * @param i columna
+     * @param j fila
+     * @return Integer
+     * Calcula si tiene 2 o mas lineas de ADN retorna verdadero sino es falso
+     * */
+    private Integer calculoDiagonalAsc(String[][] matriz, int i, int j){
         if(0<=(j-3) && matriz.length>(i+3) && matriz[j][i].equals(matriz [j-1][i+1])
                 && matriz[j][i].equals(matriz [j-2][i+2])
                 && matriz[j][i].equals(matriz [j-3][i+3])){
-            return 1L;
+            return 1;
         }
-        return 0L;
+        return 0;
     }
 
-    private Long calculoFila(String[][] matriz, int i, int j){
+    private Integer calculoFila(String[][] matriz, int i, int j){
         if(matriz.length>(i+3) && matriz[j][i].equals(matriz [j][i+1])
                 && matriz[j][i].equals(matriz [j][i+2])
                 && matriz[j][i].equals(matriz [j][i+3])){
-            return 1L;
+            return 1;
         }
-        return 0L;
+        return 0;
     }
 
-    private Long calculoDiagonalDesc(String[][] matriz, int i, int j){
+    private Integer calculoDiagonalDesc(String[][] matriz, int i, int j){
         if(matriz.length>(j+3) && matriz.length>(i+3) && matriz[j][i].equals(matriz [j+1][i+1])
                 && matriz[j][i].equals(matriz [j+2][i+2])
                 && matriz[j][i].equals(matriz [j+3][i+3])){
-            return 1L;
+            return 1;
         }
-        return 0L;
+        return 0;
     }
 
-    private Long calculoColumna(String[][] matriz, int i, int j){
+    private Integer calculoColumna(String[][] matriz, int i, int j){
         if(matriz.length>(i+3) && matriz[i][j].equals(matriz [i+1][j])
                 && matriz[i][j].equals(matriz [i+2][j])
                 && matriz[i][j].equals(matriz [i+3][j])){
-            return 1L;
+            return 1;
         }
-        return 0L;
+        return 0;
     }
 
     private String [][] getMatriz(String[] dna){
@@ -94,7 +109,6 @@ public class ValidateADNServiceImpl implements IValidateADNService {
         Boolean invalido = false;
         while(i < array.length && invalido == false){
             invalido = lineaInvalida(array[i++].toLowerCase());
-            i++;
         }
         return invalido;
     }
