9821bbae89f129b7eeb2ca42186524b6b7c88fde
Lucas Avila <lucas.avila.2010@gmail.com>
2018-05-24 00:48:34 -0300

    se agregan tests

---
+++ src/test/groovy/com/mercado/libre/Magneto/configuracion/HibernatePropertiesTest.groovy
@@ -0,0 +1,58 @@
+package com.mercado.libre.Magneto.configuracion
+
+import spock.lang.Specification
+
+public class HibernatePropertiesTest extends Specification {
+
+
+    def "hibernate properties getHbm2ddlAuto"() {
+        given:
+            HibernateProperties hibernateProperties= new HibernateProperties();
+        when:
+            def resultado = hibernateProperties.getHbm2ddlAuto()
+        then:
+            resultado == "none"
+    }
+
+    def "hibernate properties isShowSql"() {
+        given:
+            HibernateProperties hibernateProperties= new HibernateProperties();
+        when:
+            def resultado = hibernateProperties.isShowSql()
+        then:
+            resultado == true
+    }
+
+    def "hibernate properties getShowSql"() {
+        given:
+            HibernateProperties hibernateProperties= new HibernateProperties();
+        when:
+            def resultado = hibernateProperties.getShowSql()
+        then:
+            resultado == true
+    }
+
+    def "hibernate properties setShowSql"() {
+        given:
+            HibernateProperties hibernateProperties = new HibernateProperties();
+        when:
+
+            hibernateProperties.setShowSql(false)
+            def resultado = hibernateProperties.isShowSql()
+        then:
+            resultado == false
+    }
+
+
+
+    def "hibernate properties setHbm2ddlAuto"() {
+        given:
+        HibernateProperties hibernateProperties = new HibernateProperties();
+        when:
+
+        hibernateProperties.setHbm2ddlAuto("update")
+        def resultado = hibernateProperties.getHbm2ddlAuto()
+        then:
+        resultado == "update"
+    }
+}
